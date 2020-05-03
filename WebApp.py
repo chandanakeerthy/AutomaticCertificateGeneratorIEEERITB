e1c8e84369f22e2a940d1e80da82ab1627244210
ClownMonster <mk6386223@gmail.com>
2020-05-03 19:47:44 +0530

    complete  base working done

--- WebApp.py
+++ WebApp.py
@@ -1,9 +1,14 @@
 from flask import Flask , redirect, render_template, url_for, request
+import cv2
+import os
+from PIL import Image
+import glob
 
 app = Flask(__name__)
 app.static_folder = 'static'
 app.debug = True
 app.secret_key = '#$&&*()282987653ngy$$^&*$hkhgf#(*&^098765'
+
 
 
 
@@ -16,12 +21,40 @@ def certificatepage():
 @app.route('/perform', methods = ['GET','POST'])
 def perform():
     if request.method == 'POST':
-        template = request.form['template']
-        font_size = request.form['fontsize']
-        csv_file = request.form['csv']
-        
+        template = request.files['template']
+        template.save(f'./downloadFolder/{template.filename}')
+        template_file = template.filename
 
-    return render_template('home.html', content = values)
+        font_size = request.form['fontsize']
+
+        name_file = request.files['csv']
+        name_file.save(f'./downloadFolder/{name_file.filename}')
+        name_file = name_file.filename
+
+        font = cv2.FONT_HERSHEY_COMPLEX
+        fontScale = int(font_size)                 
+        color = (171, 122, 9)               
+        thickness = 5
+        names = open(f'./downloadFolder/{name_file}') # names uploaded 
+        for name in names:                    
+            text = name.upper()            
+            img = cv2.imread(f'./downloadFolder/{template_file}')
+            
+            cert_len = img.shape[1]
+            cert_mid=cert_len//2
+            txtsize = cv2.getTextSize(text, font,  fontScale, thickness)
+            txt_len=txtsize[0][0]
+            if(txt_len%2 == 0):
+                mid=txt_len//2
+            else:
+                mid=(txt_len+1)//2
+
+            org=(cert_mid-mid,450)
+            img1 = cv2.putText(img, text, org, font,  fontScale, color, thickness, cv2.LINE_AA)
+            path = r"./certificates/"        #path to save the certificates
+            cv2.imwrite(os.path.join(path , text+".jpg"), img1 )
+
+    return render_template('home.html',context="")
 
 
 if __name__ == "__main__":
--- script.py
+++ script.py
@@ -1,20 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May  1 12:09:46 2020
-
-@author: nshas
-"""
 
 import cv2
 import os 
 import xlrd 
-
-''' attach path of excel sheet containing names, if not using excel. Comment below 3 lines'''
-
-#loc = (r"")          #excel sheet path        
-#wb = xlrd.open_workbook(loc)       
-#sheet = wb.sheet_by_index(0)
-#col=                               #column number containing names
  
 font = cv2.FONT_HERSHEY_SIMPLEX     #Chnage font style,to any style supported by cv2
 fontScale = 2                       #Scale the font as per requirement
@@ -24,13 +11,13 @@ thickness = 5                       #Font Thickness
 for i in range(1):                        #use for single certificate
     text=" ".upper()             #enter name in case of single certificate, else comment
     #text=sheet.cell_value(i+1,col)
-    img=cv2.imread(r"C:\Users\nshas\Downloads\IEEE.jpeg") #attach path of cert-template
+    img=cv2.imread('./static/images/democ.jpeg') #attach path of cert-template
     
     '''underlying code, till org is applicable only if the name has to appear at mid of certificate, if name has to printed in some particular location.Calculate the org and assign accordingly. Org is the pixel value of bottom-left position of text on the image.Use the last 3 lines of code to adjust ORG'''
     
     cert_len=img.shape[1]
     cert_mid=cert_len//2
-    txtsize=cv2.getTextSize(text, font,  fontScale, thickness)
+    txtsize = cv2.getTextSize(text, font,  fontScale, thickness)
     txt_len=txtsize[0][0]
     if(txt_len%2==0):
         mid=txt_len//2
@@ -41,7 +28,7 @@ for i in range(1):                        #use for single certificate
     
     org=(cert_mid-mid,450)
     img1 = cv2.putText(img, text, org, font,  fontScale, color, thickness, cv2.LINE_AA)
-    path=r"C:\Users\nshas\Downloads\Certificates"        #path to save the certificates
+    path=r"./static/images/"        #path to save the certificates
     cv2.imwrite(os.path.join(path , text+".png"), img1 )
     
     
