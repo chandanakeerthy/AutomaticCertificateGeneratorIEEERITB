2ffb836d719bdff237a3b4084adfa576b8de16f0
ClownMonster <mk6386223@gmail.com>
2020-05-03 14:40:04 +0530

    flask inital setup

---
+++ WebApp.py
@@ -0,0 +1,19 @@
+from flask import Flask , redirect, render_template, url_for, request
+
+app = Flask(__name__)
+app.static_folder = 'static'
+app.debug = True
+app.secret_key = '#$&&*()282987653ngy$$^&*$hkhgf#(*&^098765'
+
+
+
+@app.route('/', methods = ['GET','POST'])
+@app.route('/certificate', methods = ['GET', 'POST'])
+def certificatepage():
+
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    app.run()
+    
