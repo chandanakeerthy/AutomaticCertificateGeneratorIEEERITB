e4c743446968320cbc779c40eb357d3b8bd77ae3
ClownMonster <mk6386223@gmail.com>
2020-05-05 14:17:16 +0530

    script file input handled

--- app.py
+++ app.py
@@ -18,53 +18,67 @@ app.secret_key = '#$&&*()282987653ngy$$^&*$hkhgf#(*&^098765'
 @app.route('/', methods = ['GET','POST'])
 @app.route('/certificate', methods = ['GET', 'POST'])
 def certificatepage():
-    return render_template('home.html')
+    if os.path.exists('./static/certificates.zip'):
+        os.remove('./static/certificates.zip')
+    return render_template('home.html', con = 'none')
 
 
 @app.route('/perform', methods = ['GET','POST'])
 def perform():
-    temdir = tempfile.gettempdir()
     if request.method == 'POST':
-        template_file = request.files['template']
-        template_file_name = secure_filename(template_file.filename)
-        template_file.save(os.path.join(temdir, template_file_name))
+        try:
+            temdir = tempfile.gettempdir()
+            template_file = request.files['template']
+            template_file_name = secure_filename(template_file.filename)
+            template_file.save(os.path.join(temdir, template_file_name))
 
-        font_size = request.form['fontsize']
+            font_size = request.form['fontsize']
 
-        name_file = request.files['csv']
-        name_file_name = secure_filename(name_file.filename)
-        name_file.save(os.path.join(temdir, name_file_name))
-        print(os.path.abspath(name_file_name))
+            name_file = request.files['csv']
+            name_file_name = secure_filename(name_file.filename)
+            name_file.save(os.path.join(temdir, name_file_name))
+            print(os.path.abspath(name_file_name))
 
 
-        font = cv2.FONT_HERSHEY_COMPLEX
-        fontScale = int(font_size)                 
-        color = (171, 122, 9)               
-        thickness = 5
-        names = open(f'/tmp/{name_file_name}') # names uploaded 
-        for name in names:                    
-            text = name.upper()            
-            img = cv2.imread(f'/tmp/{template_file_name}')
+            font = cv2.FONT_HERSHEY_COMPLEX
+            fontScale = int(font_size)                 
+            color = (171, 122, 9)               
+            thickness = 5
+            names = open(f'/tmp/{name_file_name}') # names uploaded 
+            for name in names:                    
+                text = name.upper()            
+                img = cv2.imread(f'/tmp/{template_file_name}')
+                cert_len = img.shape[1]
+                cert_mid=cert_len//2
+                txtsize = cv2.getTextSize(text, font,  fontScale, thickness)
+                txt_len=txtsize[0][0]
+                if(txt_len%2 == 0):
+                    mid=txt_len//2
+                else:
+                    mid=(txt_len+1)//2
 
-            cert_len = img.shape[1]
-            cert_mid=cert_len//2
-            txtsize = cv2.getTextSize(text, font,  fontScale, thickness)
-            txt_len=txtsize[0][0]
-            if(txt_len%2 == 0):
-                mid=txt_len//2
-            else:
-                mid=(txt_len+1)//2
+                org=(cert_mid-mid,450)
+                img1 = cv2.putText(img, text, org, font,  fontScale, color, thickness, cv2.LINE_AA)
+                path = r"/tmp/"        #path to save the certificates
+                cv2.imwrite(os.path.join(path , text+".png"), img1 )
 
-            org=(cert_mid-mid,450)
-            img1 = cv2.putText(img, text, org, font,  fontScale, color, thickness, cv2.LINE_AA)
-            path = r"/tmp/"        #path to save the certificates
-            cv2.imwrite(os.path.join(path , text+".png"), img1 )
-            # compressing to zip to upload
-            shutil.make_archive('./static/certificates', 'zip', '/tmp')
+                # compressing to zip to upload
+                shutil.make_archive('./static/certificates', 'zip', '/tmp')
 
-        return render_template('home.html', c = 'certificates.zip') # returning cerificates in zip to download
-    return render_template('home.html', certificates = '')
+            return render_template('home.html', c = 'certificates.zip', con = 'block') # returning cerificates in zip to download
+        except:
+            return redirect(request.url)
+    return render_template('home.html', certificates = '', con = 'none')
 
+
+#############################################################################################
+# Invalid urls routings handled
+@app.errorhandler(404)
+def page_not_found(error):
+    # future developemt to render custom 404 error page
+    return 'This Page Does Not Exists',404
+
+#############################################################################################
 
 if __name__ == "__main__":
     app.run(debug=True)
