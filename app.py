d51dfe1b3229f79693820e1dcb41589ac522ff93
ClownMonster <mk6386223@gmail.com>
2020-05-05 12:00:12 +0530

    changed the directories to tmp

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask , redirect, render_template, url_for, request, flash, send_file
 import cv2
 import os
+import tempfile
+from werkzeug.utils import secure_filename
 
 # to zip the directory
 import shutil
@@ -21,27 +23,28 @@ def certificatepage():
 
 @app.route('/perform', methods = ['GET','POST'])
 def perform():
+    temdir = tempfile.gettempdir()
     if request.method == 'POST':
-        template = request.files['template']
-        template.save(f'./static/downloadFolder/{template.filename}')
-        template.close()
-        template_file = template.filename
+        template_file = request.files['template']
+        template_file_name = secure_filename(template_file.filename)
+        template_file.save(os.path.join(temdir, template_file_name))
 
         font_size = request.form['fontsize']
 
         name_file = request.files['csv']
-        name_file.save(f'./static/downloadFolder/{name_file.filename}')
-        name_file.close()
-        name_file = name_file.filename
+        name_file_name = secure_filename(name_file.filename)
+        name_file.save(os.path.join(temdir, name_file_name))
+        print(os.path.abspath(name_file_name))
+
 
         font = cv2.FONT_HERSHEY_COMPLEX
         fontScale = int(font_size)                 
         color = (171, 122, 9)               
         thickness = 5
-        names = open(f'./downloadFolder/{name_file}') # names uploaded 
+        names = open(f'/tmp/{name_file_name}') # names uploaded 
         for name in names:                    
             text = name.upper()            
-            img = cv2.imread(f'./static/downloadFolder/{template_file}')
+            img = cv2.imread(f'/tmp/{template_file_name}')
 
             cert_len = img.shape[1]
             cert_mid=cert_len//2
@@ -54,10 +57,10 @@ def perform():
 
             org=(cert_mid-mid,450)
             img1 = cv2.putText(img, text, org, font,  fontScale, color, thickness, cv2.LINE_AA)
-            path = r"./static/certificates/"        #path to save the certificates
+            path = r"/tmp/"        #path to save the certificates
             cv2.imwrite(os.path.join(path , text+".png"), img1 )
             # compressing to zip to upload
-            shutil.make_archive('./static/certificates', 'zip', './static/certificates')
+            shutil.make_archive('./static/certificates', 'zip', '/tmp')
 
         return render_template('home.html', c = 'certificates.zip') # returning cerificates in zip to download
     return render_template('home.html', certificates = '')
