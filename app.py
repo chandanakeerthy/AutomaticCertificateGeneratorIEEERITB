9ca45554e4b5b0c0de6f4f9d0d0e9615c9d0d217
ClownMonster <mk6386223@gmail.com>
2020-05-05 11:05:12 +0530

    Changed the folders into static

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ def certificatepage():
 def perform():
     if request.method == 'POST':
         template = request.files['template']
-        template.save(f'./downloadFolder/{template.filename}')
+        template.save(f'./static/downloadFolder/{template.filename}')
         template_file = template.filename
 
         font_size = request.form['fontsize']
 
         name_file = request.files['csv']
-        name_file.save(f'./downloadFolder/{name_file.filename}')
+        name_file.save(f'./static/downloadFolder/{name_file.filename}')
         name_file = name_file.filename
 
         font = cv2.FONT_HERSHEY_COMPLEX
@@ -39,7 +39,7 @@ def perform():
         names = open(f'./downloadFolder/{name_file}') # names uploaded 
         for name in names:                    
             text = name.upper()            
-            img = cv2.imread(f'./downloadFolder/{template_file}')
+            img = cv2.imread(f'./static/downloadFolder/{template_file}')
 
             cert_len = img.shape[1]
             cert_mid=cert_len//2
@@ -52,10 +52,10 @@ def perform():
 
             org=(cert_mid-mid,450)
             img1 = cv2.putText(img, text, org, font,  fontScale, color, thickness, cv2.LINE_AA)
-            path = r"./certificates/"        #path to save the certificates
+            path = r"./static/certificates/"        #path to save the certificates
             cv2.imwrite(os.path.join(path , text+".png"), img1 )
             # compressing to zip to upload
-            shutil.make_archive('./static/certificates', 'zip', './certificates')
+            shutil.make_archive('./static/certificates', 'zip', './static/certificates')
 
         return render_template('home.html', c = 'certificates.zip') # returning cerificates in zip to download
     return render_template('home.html', certificates = '')
